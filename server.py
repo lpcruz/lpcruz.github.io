d97d3f74b30d8bae50b7fb6e26873bd3adf06fc1
Laurence Cruz <lcruz@squarespace.com>
2016-08-24 12:28:20 -0400

    Starts wiring everything

---
+++ server.py
@@ -0,0 +1,16 @@
+import SimpleHTTPServer
+import SocketServer
+
+# minimal web server.  serves files relative to the
+# current directory.
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "Serving at port", PORT
+print "Navigate to http://localhost:8000"
+
+httpd.serve_forever()
